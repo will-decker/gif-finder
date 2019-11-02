1bfa1f02f23a1fd09c7adb651528db3ff2a0e880
William Decker <willdecker@gmail.com>
2019-11-02 14:53:34 -0400

    Add search function and update style of gif grid. Refactor API request code

--- gifFinder.js
+++ gifFinder.js
@@ -1,41 +1,56 @@
-var searchBox = document.getElementById("searchInput");
-var submitBtn = document.getElementById("submitBtn");
-var apiKey = "dc6zaTOxFJmzC";
-var searchEndPoint = "http://api.giphy.com/v1/gifs/search?";
-var trendingEndPoint = "http://api.giphy.com/v1/gifs/trending?";
-var searchUrl = searchEndPoint + "api_key=" + apiKey;
-var trendingUrl = trendingEndPoint + "api_key=" + apiKey;
+var searchBox = document.getElementById('searchInput');
+var searchBtn = document.getElementById('searchBtn');
+var searchForm = document.getElementById('searchForm');
+var apiKey = 'dc6zaTOxFJmzC';
+var searchTerm = '';
+const giphyAPI = 'http://api.giphy.com/v1/gifs/';
+var searchEndPoint = `${giphyAPI}search?`;
+var trendingEndPoint = 'http://api.giphy.com/v1/gifs/trending?';
+var searchUrl = searchEndPoint + 'api_key=' + apiKey + '&limit=25&q=';
+var trendingUrl = trendingEndPoint + 'api_key=' + apiKey;
 
-window.onload = getTrendingGifs;
+init();
 
 function getTrendingGifs() {
+  gifRequest(trendingUrl);
+}
+
+searchBtn.addEventListener('click', findGifs);
+
+function findGifs() {
+  searchTerm = searchBox.value;
+  console.log(searchTerm);
+  gifRequest(searchUrl + searchTerm);
+}
+
+function gifRequest(url) {
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', trendingUrl, true)
+  xhr.open('GET', url, true);
 
-  xhr.onload = function () {
+  xhr.onload = function() {
     if (this.status == 200) {
-
       var gifs = JSON.parse(this.responseText);
-      var output = "";
+      var output = '';
 
       console.log(gifs.data.length);
 
       for (var i = 0; i < gifs.data.length; i++) {
         console.log(gifs.data[i]);
         var gifImage = gifs.data[i].images.original.url;
-        output += '<img src="' + gifImage + '">'
+        output += '<img src="' + gifImage + '">';
       }
     }
     document.getElementById('gif_grid').innerHTML = output;
-  }
+  };
   xhr.send();
 }
 
+// function clearSearch() {
+//   searchForm.reset();
+//   searchTerm = '';
+// }
 
-submitBtn.addEventListener("click", findGifs);
-
-function findGifs() {
-  var searchTerm = searchBox.value
-  console.log();
+function init() {
+  getTrendingGifs();
+  // clearSearch();
 }
-

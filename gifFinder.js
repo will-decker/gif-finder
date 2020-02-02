b1adfb5ed0f5fa6b94c657fdfc372fc4d6e76676
William Decker <willdecker@gmail.com>
2020-02-02 16:00:36 -0500

    Refactor code to append GIFs to DOM. Add basic CSS styling to app. Testing infinite scroll functionality

--- gifFinder.js
+++ gifFinder.js
@@ -1,56 +1,82 @@
-var searchBox = document.getElementById('searchInput');
-var searchBtn = document.getElementById('searchBtn');
-var searchForm = document.getElementById('searchForm');
-var apiKey = 'dc6zaTOxFJmzC';
-var searchTerm = '';
+const searchBox = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
+const searchForm = document.getElementById('searchForm');
+const gifContainer = document.getElementById('gif-grid');
+const trending = document.getElementById('trending');
+
+const apiKey = 'wgMoDU1mKp8t8xdPjuRvigmzMXxnKSyM';
+let searchTerm = '';
 const giphyAPI = 'http://api.giphy.com/v1/gifs/';
-var searchEndPoint = `${giphyAPI}search?`;
-var trendingEndPoint = 'http://api.giphy.com/v1/gifs/trending?';
-var searchUrl = searchEndPoint + 'api_key=' + apiKey + '&limit=25&q=';
-var trendingUrl = trendingEndPoint + 'api_key=' + apiKey;
+const searchEndPoint = `${giphyAPI}search?`;
+const trendingEndPoint = `${giphyAPI}trending?`;
+let searchUrl = searchEndPoint + 'api_key=' + apiKey + '&limit=40&q=';
+let trendingUrl = trendingEndPoint + 'api_key=' + apiKey + '&limit=40';
 
 init();
 
-function getTrendingGifs() {
-  gifRequest(trendingUrl);
+searchBtn.addEventListener('click', findGIFs);
+trending.addEventListener('click', trendingBtn);
+
+function addGIFToDOM(imgSrc) {
+  const GIF = document.createElement('div');
+  GIF.innerHTML = `<img src="${imgSrc}">`;
+  gifContainer.appendChild(GIF);
 }
 
-searchBtn.addEventListener('click', findGifs);
+function getTrendingGIFs() {
+  getGIFs(trendingUrl);
+}
 
-function findGifs() {
+function trendingBtn() {
+  clearDOM();
+  getTrendingGIFs();
+}
+
+function findGIFs() {
   searchTerm = searchBox.value;
   console.log(searchTerm);
-  gifRequest(searchUrl + searchTerm);
+  clearDOM();
+  getGIFs(searchUrl + searchTerm);
+  // clearSearch();
 }
 
-function gifRequest(url) {
-  var xhr = new XMLHttpRequest();
+function getGIFs(url) {
+  const xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
 
   xhr.onload = function() {
     if (this.status == 200) {
-      var gifs = JSON.parse(this.responseText);
-      var output = '';
+      const dataGIFs = JSON.parse(this.responseText);
 
-      console.log(gifs.data.length);
-
-      for (var i = 0; i < gifs.data.length; i++) {
-        console.log(gifs.data[i]);
-        var gifImage = gifs.data[i].images.original.url;
-        output += '<img src="' + gifImage + '">';
-      }
+      addGIFsToDOM(dataGIFs);
     }
-    document.getElementById('gif_grid').innerHTML = output;
   };
   xhr.send();
 }
 
-// function clearSearch() {
-//   searchForm.reset();
-//   searchTerm = '';
-// }
-
-function init() {
-  getTrendingGifs();
-  // clearSearch();
+function clearSearch() {
+  searchInput.value = '';
 }
+
+function init() {}
+
+function clearDOM() {
+  while (gifContainer.firstChild) {
+    gifContainer.removeChild(gifContainer.firstChild);
+  }
+}
+
+function addGIFsToDOM(srcGIFs) {
+  for (var i = 0; i < srcGIFs.data.length; i++) {
+    var imgGIF = srcGIFs.data[i].images.original.url;
+    addGIFToDOM(imgGIF);
+  }
+}
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (clientHeight + scrollTop >= scrollHeight - 5) {
+    getGIFs(searchUrl + searchTerm + '&offset=41');
+  }
+});
